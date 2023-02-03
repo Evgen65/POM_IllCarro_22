9d29d6adeb14d843637723d343ce7e4fcf0a0cf3
Evgen65 <uevgeny1965@gmail.com>
2023-02-03 19:03:23 +0200

    finally

---
+++ src/test/java/tests/RemoveCarTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import config.AppiumConfig;
+import models.Auth;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import screens.SearchScreen;
+
+public class RemoveCarTest extends AppiumConfig {
+    @BeforeMethod
+    public void preCondition(){
+        new SearchScreen(driver)
+                .openMoreOptions()
+                .openFormLogin()
+                .fillLoginForm(Auth.builder().email("noa@gmail.com").password("Nnoa12345$").build())
+                .submitLogin();
+    }
+
+    @Test
+    public void removeCarSuccessTest(){
+        new SearchScreen(driver)
+                .openMoreOptions()
+                .openMyCarsList()
+                .removeCar();
+    }
+}
+
